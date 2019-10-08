95f12e8ceac44f0c5678760fd0818302ef4592ec
minami-choi <cmina2846@gmail.com>
2019-10-09 03:10:29 +0900

    blockchain.js complete

---
+++ src/blockchain.js
@@ -0,0 +1,146 @@
+const CryptoJS = require("crypto-js");
+
+// 1. block 클래스 정의
+class Block {
+  constructor(index, hash, prevHash, timestamp, data) {
+    this.index = index;
+    this.hash = hash;
+    this.prevHash = prevHash;
+    this.timestamp = timestamp;
+    this.data = data;
+  }
+}
+
+// 2. genesis block은 하드코딩
+// 첫번째 블록해시로 01570555253933This is the genesis!! 을 해시한 값을 넣어준다
+const genesisBlock = new Block(
+  0,
+  "49366cfdf758988a6d44037ea8827e018044b7e41aae84cf02356ffbbbd62e88",
+  null,
+  new Date().getTime() / 1000,
+  "This is the genesis!!"
+);
+
+let blockchain = [genesisBlock];
+
+console.log(blockchain);
+
+// todo 함수형으로 작성
+// 4. 가장최근 블록 가져오기
+const getLastBlock = () => blockchain[blockchain.length - 1];
+// 아래함수를 ES6로 나타내면 위와 같이
+// function getLastBlock() {
+//     return blockchain[blockchain.length - 1]
+// }
+
+// 5. 현재타임스탬프구하는 함수
+const getTimeStamp = () => new Date().getTime() / 1000;
+
+const getBlockchain = () => blockchain;
+
+// 6. 해시함수
+const createHash = (index, prevHash, timestamp, data) =>
+  CryptoJS.SHA256(index + prevHash + timestamp + data);
+
+// 3. 함수만들기
+const createNewBlock = data => {
+  const prevBlock = getLastBlock();
+  const newBlockIndex = prevBlock.index + 1;
+  const newTimeStamp = getTimeStamp();
+  const newHash = createHash(newBlockIndex, prevBlock.hash, newTimeStamp, data);
+
+  const newBlock = new Block(
+    newBlockIndex,
+    newHash,
+    prevBlock.hash,
+    newTimeStamp,
+    data
+  );
+  return newBlock;
+};
+
+// 8. 검증에 사용할 블록해시
+const getBlocksHash = block =>
+  createHash(
+    block.index,
+    block.prevHash,
+    block.timestamp,
+    JSON.stringify(block.data)
+  );
+
+// 9. 블록 구조 검증(각데이터의 타입검증)
+const isNewStructureValid = block => {
+  return (
+    typeof block.index === "number" &&
+    typeof block.hash === "string" &&
+    typeof block.prevHash === "string" &&
+    typeof block.timestamp === "number" &&
+    typeof block.data === "string"
+  );
+};
+
+// 7. 블록 유효성 검증
+const isNewBlockValid = (candidateBlock, latestBlock) => {
+  if (!isNewStructureValid) {
+    console.log("The candidate block structure is not valid");
+    return false;
+  } else if (latestBlock.index + 1 !== candidateBlock.index) {
+    console.log("The candidate block doesn't have a valid index");
+    return false;
+  } else if (latestBlock.hash !== candidateBlock.prevHash) {
+    console.log(
+      "The previousHash of the candidate block is not the hash of the latest block"
+    );
+    return false;
+  } else if (getBlocksHash(candidateBlock) !== candidateBlock.hash) {
+    console.log("The hash of this block is invalid");
+    return false;
+  }
+  return true;
+};
+
+// todo 이 과정이 언제, 어디서, 어떻게 일어나는거지?
+// todo reorg 그림에서보면 중간에 바뀌는 걸로 보이는데, 여기 코드상으로는 체인 전체가 replace?
+// 10.chain valid - 들어오는 블록들이 valid한지 체크해야함, 길이가 긴 체인으로 스위칭되기도 함.
+// 같은 제네시스 출신의 체인이어야함
+const isChainValid = candidateChain => {
+  const isGenesisValid = block =>
+    JSON.stringify(block) === JSON.stringify(genesisBlock);
+
+  if (!isGenesisValid(candidateChain[0])) {
+    console.log(
+      "The candidate chain's genesisblock is not the same as our genesisblock"
+    );
+    return false;
+  }
+
+  for (let i = 1; i < candidateChain.length; i++) {
+    if (!isNewBlockValid(candidateChain[i], candidateChain[i - 1])) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// 11. 만약 새로 들어온 체인이 유효하다면 replace해주는 기능필요
+const replaceChain = candidateChain => {
+  if (
+    isChainValid(candidateChain) &&
+    candidateChain.length > getBlockchain().length
+  ) {
+    blockchain = candidateChain;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// 12. 새로운블록 체인에 추가하기
+const addBlockToChain = candidateBlock => {
+  if (isNewBlockValid(candidateBlock, getLastBlock)) {
+    getBlockchain().push(candidateBlock);
+    return true;
+  } else {
+    return false;
+  }
+};
