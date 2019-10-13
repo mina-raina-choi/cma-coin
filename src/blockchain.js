7694fa4cd0bfd64d1b58075534a425a5610b5a60
minami-choi <cmina2846@gmail.com>
2019-10-13 20:00:06 +0900

    1. apply difficulty, nonce 2. block timestamp valid check

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,13 +1,21 @@
-const CryptoJS = require("crypto-js");
+const CryptoJS = require("crypto-js"),
+  hexToBinary = require("hex-to-binary");
+
+//   블록생성주기 10초
+const BLOCK_GENERATION_INTERVAL = 10;
+// 얼마나 자주 채굴난이도를 조절할 것인지, 비트코인은 2016블록마다
+const DIFFICULTY_ADJUSTMENT_INTERVAL = 10;
 
 // 1. block 클래스 정의
 class Block {
-  constructor(index, hash, prevHash, timestamp, data) {
+  constructor(index, hash, prevHash, timestamp, data, difficulty, nonce) {
     this.index = index;
     this.hash = hash;
     this.prevHash = prevHash;
     this.timestamp = timestamp;
     this.data = data;
+    this.difficulty = difficulty;
+    this.nonce = nonce;
   }
 }
 
@@ -15,10 +23,12 @@ class Block {
 // 첫번째 블록해시로 01570595637361This is the genesis!! 을 해시한 값을 넣어준다
 const genesisBlock = new Block(
   0,
-  "dc90cc2eb3ea0dff4fcf6c0b9fe0954cb8bbe050ebee9d0402ca31551766466d",
+  "ef14c074cefb782f2170b5218ebf1122cae87e60f5f6324f0e17f4d72d1b5ea8",
   null,
-  1570595637361,
-  "This is the genesis!!"
+  1570963704,
+  "This is the genesis!!",
+  0,
+  0
 );
 
 let blockchain = [genesisBlock];
@@ -34,34 +44,31 @@ const getNewestBlock = () => blockchain[blockchain.length - 1];
 // }
 
 // 5. 현재타임스탬프구하는 함수
-const getTimeStamp = () => new Date().getTime() / 1000;
+const getTimeStamp = () => Math.round(new Date().getTime() / 1000);
 
 const getBlockchain = () => blockchain;
 
 // 6. 해시함수
-const createHash = (index, prevHash, timestamp, data) =>
-  CryptoJS.SHA256(index + prevHash + timestamp + data).toString();
+const createHash = (index, prevHash, timestamp, data, difficulty, nonce) =>
+  CryptoJS.SHA256(
+    index + prevHash + timestamp + JSON.stringify(data) + difficulty + nonce
+  ).toString();
 
 // 3. 함수만들기
 const createNewBlock = data => {
   const prevBlock = getNewestBlock();
   const newBlockIndex = prevBlock.index + 1;
   const newTimeStamp = getTimeStamp();
-  const newHash = createHash(newBlockIndex, prevBlock.hash, newTimeStamp, data);
-  console.log(
-    "newHash",
-    newHash,
+
+  const difficulty = findDifficulty();
+  //   new Block 대신에 findBlock
+  const newBlock = findBlock(
     newBlockIndex,
     prevBlock.hash,
     newTimeStamp,
-    data
-  );
-  const newBlock = new Block(
-    newBlockIndex,
-    newHash,
-    prevBlock.hash,
-    newTimeStamp,
-    data
+    data,
+    // how to calculate difficulty
+    difficulty
   );
 
   addBlockToChain(newBlock);
@@ -72,10 +79,93 @@ const createNewBlock = data => {
   return newBlock;
 };
 
+// 난이도 조절
+const findDifficulty = () => {
+  const blockchain = getBlockchain();
+  const newestBlock = blockchain[blockchain.length - 1];
+  // 현재블록높이가 10으로 나누어떨어지면
+  if (
+    newestBlock.index % DIFFICULTY_ADJUSTMENT_INTERVAL === 0 &&
+    // genesis
+    newestBlock.index != 0
+  ) {
+    //   calculate new difficulty
+    return calculateNewDifficulty(newestBlock, blockchain);
+  } else {
+    //  현재의 난이도를 그대로 리턴
+    return newestBlock.difficulty;
+  }
+};
+
+const calculateNewDifficulty = (newestBlock, blockchain) => {
+  const lastCalculatedBlock =
+    //   10 블록 전의 블록
+    blockchain[blockchain.length - DIFFICULTY_ADJUSTMENT_INTERVAL];
+  const timeExpected =
+    BLOCK_GENERATION_INTERVAL * DIFFICULTY_ADJUSTMENT_INTERVAL;
+  // 누군가 timestamp를 조정하면 난이도를 막 조정할 수 있다. => 이상한 값을 넣으면 안되도록 유효성검증이 필요
+  const timeTaken = newestBlock.timestamp - lastCalculatedBlock.timestamp;
+  // timeExpected = 100
+  if (timeTaken < timeExpected / 2) {
+    // 예상시간의 1/2보다 더 적게 걸렸으면, timeTaken < 50
+    return lastCalculatedBlock.difficulty + 1;
+  } else if (timeTaken > timeExpected * 2) {
+    // 예상시간보다 2배이상 더 걸렸으면, timeTaken > 200
+    return lastCalculatedBlock.difficulty - 1;
+  } else {
+    //   50 ~ 200 사이
+    return lastCalculatedBlock.difficulty;
+  }
+};
+
+const findBlock = (index, prevHash, timestamp, data, difficulty) => {
+  let nonce = 0;
+  while (true) {
+    console.log(`current nonce : ${nonce}`);
+
+    const hash = createHash(
+      index,
+      prevHash,
+      timestamp,
+      data,
+      difficulty,
+      nonce
+    );
+
+    // todo check amount of zeros
+    if (hashMatchesDifficulty(hash, difficulty)) {
+      return new Block(
+        index,
+        hash,
+        prevHash,
+        timestamp,
+        data,
+        difficulty,
+        nonce
+      );
+    }
+    nonce++;
+  }
+};
+
+const hashMatchesDifficulty = (hash, difficulty) => {
+  // hash -> binary
+  const hashInBinary = hexToBinary(hash);
+  const requiredZeros = "0".repeat(difficulty);
+  console.log("Trying difficulty: ", difficulty, " with hash: ", hashInBinary);
+  return hashInBinary.startsWith(requiredZeros);
+};
+
 // 8. 검증에 사용할 블록해시
 const getBlocksHash = block =>
-  createHash(block.index, block.prevHash, block.timestamp, block.data);
-
+  createHash(
+    block.index,
+    block.prevHash,
+    block.timestamp,
+    block.data,
+    block.difficulty,
+    block.nonce
+  );
 // 9. 블록 구조 검증(각데이터의 타입검증)
 const isBlockStructureValid = block => {
   return (
@@ -84,6 +174,20 @@ const isBlockStructureValid = block => {
     typeof block.prevHash === "string" &&
     typeof block.timestamp === "number" &&
     typeof block.data === "string"
+  );
+};
+
+// 타임스탬프가 과거의 1분 ~ 미래의 1분 사이일 때 유효
+const isTimeStampValid = (newBlock, oldBlock) => {
+  console.log("newBlock.timestamp : ", newBlock.timestamp);
+  console.log("oldBlock.timestamp : ", oldBlock.timestamp);
+  console.log("getTimeStamp : ", getTimeStamp());
+
+  return (
+    //   그냥 oldBlock.timestamp < newBlock.timestamp 이면 안되나?
+    // newBlock.timestamp < getTimeStamp()
+    oldBlock.timestamp < newBlock.timestamp &&
+    newBlock.timestamp - 60 < getTimeStamp()
   );
 };
 
@@ -105,11 +209,10 @@ const isBlockValid = (candidateBlock, latestBlock) => {
     );
     return false;
   } else if (getBlocksHash(candidateBlock) !== candidateBlock.hash) {
-    console.log(
-      "The hash of this block is invalid",
-      getBlocksHash(candidateBlock),
-      candidateBlock
-    );
+    console.log("The hash of this block is invalid");
+    return false;
+  } else if (!isTimeStampValid(candidateBlock, latestBlock)) {
+    console.log("The timestamp of this block is invalid");
     return false;
   }
   return true;
